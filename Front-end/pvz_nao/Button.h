0c22695ba8bcf5194ccda7b3dcc340043f9c9cf6
Suhp <351675021@qq.com>
2025-06-30 17:34:25 +0800

    Add files via upload

---
+++ Front-end/pvz_nao/Button.h
@@ -0,0 +1,47 @@
+#pragma once
+#include<graphics.h>
+
+//这个文件夹中包含了按钮基类，还有所有类型的按钮
+
+
+class Button
+{
+public:
+	Button(RECT rect, LPCTSTR path_img_idle, LPCSTR path_img_hovered);
+
+	~Button() = default;
+
+	void ProcessEvent(const ExMessage& msg);
+	void Draw();
+
+protected:
+	virtual void OnClick() = 0;
+
+private:
+	enum class Status
+	{
+		Idle = 0,
+		Hovered,
+		Pushed
+	};
+	RECT region;
+	IMAGE img_idle;
+	IMAGE img_hovered;
+	Status status = Status::Idle;
+
+	bool CheckCursorHit(int x, int y)
+	{
+		return x >= region.left && x <= region.right && y >= region.top && y <= region.bottom;
+	}
+};
+
+class startButton:public Button
+{ 
+public:
+	startButton(RECT rect, LPCTSTR path_img_idle, LPCSTR path_img_hovered) :Button(rect, path_img_idle, path_img_hovered) {};
+	~startButton() = default;
+	void OnClick();
+
+	bool is_game_started = false;
+	
+};
---
+++ Front-end/pvz_nao/movingScene.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<graphics.h>
+#include"PlayScene.h"
+#include"res.h"
+#include"tools.h"
+#include"Button.h"
+extern MyRes RESALL;
+
+void moving_scene();             //ÒÆ¶¯³¡¾°
